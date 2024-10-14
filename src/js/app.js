40b9b6a112fd89cbf91fc230eddf6b99b3ffdd5d
Pauchoook <nkolatarasov@mail.ru>
2024-10-15 01:47:59 +0300

    full

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import 'aos/dist/aos.css';
+import "../scss/style.scss";
+import AOS from 'aos';
+import anchor from "./files/anchor.js";
+import fancy from "./files/fancy.js";
+import sliders from "./files/sliders.js";
+
+sliders();
+fancy();
+anchor();
+
+AOS.init();
